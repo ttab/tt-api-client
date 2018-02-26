5eab7ba36d385787d90c46a6eb1c1e04dcfb16cb
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2018-02-26 15:17:19 +0100

    update for content-api

--- api.js
+++ api.js
@@ -1,8 +1,47 @@
 module.exports = function (EventEmitter, request) {
-  var Client = require('./lib/push-client')(EventEmitter, request)
-  return {
-    pushClient: function (opts) {
-      return new Client(opts)
+  return function (opts) {
+    opts = Object.assign({host: 'http://localhost:3100'}, opts)
+    const rest = function (mt, op, q) {
+      return new Promise(function (resolve, reject) {
+        request.get({
+          url: opts.host + '/content/v1/' + mt + '/' + op,
+          qs: q,
+          auth: {bearer: opts.token},
+          strictSSL: false
+        }, function (err, response, body) {
+          if (err) return reject(err)
+          if (response.statusCode === 200) {
+            return resolve(JSON.parse(body))
+          }
+          reject(new Error('status ' + response.statusCode))
+        })
+      })
+    }
+
+    return {
+      content: function (mediaType) {
+        return {
+          search: function (q) {
+            return rest(mediaType, 'search', q)
+          },
+          stream: function (q) {
+            var events = new EventEmitter()
+            var next = function () {
+              rest(mediaType, 'stream', q).then(function (hits) {
+                hits.hits.forEach(function (hit) {
+                  events.emit('hit', hit)
+                })
+              }).catch(function (err) {
+                console.error(err)
+              }).then(function () {
+                return next()
+              })
+            }
+            next()
+            return events
+          }
+        }
+      }
     }
   }
 }
